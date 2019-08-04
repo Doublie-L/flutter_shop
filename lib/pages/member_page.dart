1f49560065f61b57c1e53b2e7e5d2c25d3cbb2e8
刘亮 <oncwnuCt8t9Jz9-jml3sApX8MY-M@git.weixin.qq.com>
2019-08-04 23:38:10 +0800

    初始化仓库

---
+++ lib/pages/member_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MemberPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('这是会员页面'),
+      ),
+    );
+  }
+}
